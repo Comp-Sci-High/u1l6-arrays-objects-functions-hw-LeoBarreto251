a63170a3560457ddcd7287d8232bc4d17ae37d35
LeoBarreto251 <leandro.barreto26@compscihigh.org>
2025-09-18 15:48:17 +0000

    Completed Task 1

--- index.js
+++ index.js
@@ -9,7 +9,12 @@
 // E. Delete the last fruit in the array.
 // F. Log the updated array.
 
-
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits[1]);
+fruits[2] = "orange";
+fruits.push("grape");
+fruits.pop();
+console.log(fruits);
 
 // TASK 2: Objects - Create, Read, Update, Delete (CRUD)
 // A. Create an object called `student` with the keys: name, age, and grade. (add your own values)
